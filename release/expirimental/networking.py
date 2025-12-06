be14b7d9c276b9e7623c771036bf9254e95f4251
m <112826187+chromeheartless@users.noreply.github.com>
2025-12-06 00:50:49 +0000

    Deleted old code, fixed file paths for images. Preparing for the full upload.

--- release/expirimental/networking.py
+++
@@ -1,9 +0,0 @@
-# import requests
-import os
-import json
-import random
-import time
-from datetime import datetime
-
-
-SERVER_HOST_IP = "120.0.0.1"
--- src/network/main.py
+++
@@ -1,5 +0,0 @@
-from encryption import *
-import requests
-
-HOST_PORT = 420
-HOST_IP = "120.0.0.1"
